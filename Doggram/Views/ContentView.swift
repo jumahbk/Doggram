dd1831df9c6eb9cbbf5fabe0aa7ae90750600cfc
Bander Jumah <bander@medartclinics.com>
2023-06-10 23:24:08 +0300

    kkkkk

---
+++ Doggram/Views/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Doggram
+//
+//  Created by Bander Jumah on 21/11/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Text("Screen 1").tabItem {
+                Image(systemName: "book.fill")
+                Text("tab 1")
+            }
+            Text("Screen 2").tabItem {
+                Image(systemName: "book.fill")
+                Text("Browse")
+            }
+            Text("Screen 3").tabItem {
+                Image(systemName: "book.fill")
+                Text("tab 1")
+            }
+            Text("Screen 1").tabItem {
+                Image(systemName: "book.fill")
+                Text("tab 1")
+            }
+        }.accentColor(.green)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Doggram/helpers/Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Extensions.swift
+//  Doggram
+//
+//  Created by Bander Jumah on 21/11/1444 AH.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    
+    struct MyTheme{
+        static var purpleColor : Color{
+            return Color("Purple")
+        }
+        static var yellowColor : Color{
+            return Color("Yellow")
+        }
+    }
+    
+}
