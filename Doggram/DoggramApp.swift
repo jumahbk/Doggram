d63bcd485850fd0454774912a94282862057d489
Bander Jumah <bander@medartclinics.com>
2023-06-10 22:14:10 +0300

    Initial Commit

---
+++ Doggram/DoggramApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoggramApp.swift
+//  Doggram
+//
+//  Created by Bander Jumah on 21/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct DoggramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
